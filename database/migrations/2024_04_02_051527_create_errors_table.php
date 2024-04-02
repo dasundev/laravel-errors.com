ed6272023e183b269395d4ad8213b6526318c40d
Dasun Tharanga <hello@dasun.dev>
2024-04-02 14:10:29 +0530

    feat: add slug to errors table

--- database/migrations/2024_04_02_051527_create_errors_table.php
+++ database/migrations/2024_04_02_051527_create_errors_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('errors', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('slug')->unique();
             $table->string('exception');
             $table->longText('solution');
             $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
