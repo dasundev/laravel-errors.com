d2314e476b0e61b1a91c1618fa6dabaeacea50ef
Dasun Tharanga <hello@dasun.dev>
2024-04-02 11:08:51 +0530

    feat: implements has color and has label interfaces

--- app/Enums/ErrorStatus.php
+++ app/Enums/ErrorStatus.php
@@ -2,9 +2,27 @@
 
 namespace App\Enums;
 
-enum ErrorStatus: string
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasLabel;
+use Illuminate\Support\Str;
+
+enum ErrorStatus: string implements HasColor, HasLabel
 {
     case Pending = 'pending';
     case Approved = 'approved';
     case Rejected = 'rejected';
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::Pending => 'warning',
+            self::Approved => 'success',
+            self::Rejected => 'danger',
+        };
+    }
+
+    public function getLabel(): ?string
+    {
+        return Str::ucfirst($this->value);
+    }
 }
