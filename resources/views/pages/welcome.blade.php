a23be9f0611ca3b691c9c3698f7c852213822651
Dasun Tharanga <hello@dasun.dev>
2024-04-02 20:41:44 +0530

    feat: change the modal transition duration

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -28,16 +28,16 @@ state(['latestErrors' => Error::latest()->take(3)->get()]);
                     <template x-teleport="body">
                         <div x-show="modalOpen" class="fixed top-0 left-0 z-[99] flex items-center justify-center w-screen h-screen" x-cloak>
                             <div x-show="modalOpen"
-                                 x-transition:enter="ease-out duration-300"
+                                 x-transition:enter="ease-out duration-200"
                                  x-transition:enter-start="opacity-0"
                                  x-transition:enter-end="opacity-100"
-                                 x-transition:leave="ease-in duration-300"
+                                 x-transition:leave="ease-in duration-200"
                                  x-transition:leave-start="opacity-100"
                                  x-transition:leave-end="opacity-0"
                                  @click="modalOpen=false" class="absolute inset-0 w-full h-full bg-black bg-opacity-75"></div>
                             <div x-show="modalOpen"
                                  x-trap.inert.noscroll="modalOpen"
-                                 x-transition:enter="ease-out duration-300"
+                                 x-transition:enter="ease-out duration-200"
                                  x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                                  x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
                                  x-transition:leave="ease-in duration-200"
