d66107913a31e633ac07ba0bc8e51cf2c8d1bb97
Dasun Tharanga <hello@dasun.dev>
2024-04-02 23:23:42 +0530

    feat: delete admin panel

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
