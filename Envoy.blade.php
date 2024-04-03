9a9d9726c3eb11bc3fcf19db8c4a450ed7752dbe
Dasun Tharanga <hello@dasun.dev>
2024-04-03 13:52:20 +0530

    feat: install laravel envoy

---
+++ Envoy.blade.php
@@ -0,0 +1,66 @@
+@servers(['web' => $host])
+
+@story('deploy')
+maintenance-mode-on
+update-code
+install-dev-dependencies
+migrate-database
+build-assets
+install-prod-dependencies
+optimize
+reload-octane-server
+maintenance-mode-off
+@endstory
+
+@task('update-code')
+cd {{ $path }}
+git pull origin main
+@endtask
+
+@task('install-dev-dependencies')
+cd {{ $path }}
+composer install
+npm install
+@endtask
+
+@task('install-prod-dependencies')
+cd {{ $path }}
+composer install --optimize-autoloader --no-dev
+@endtask
+
+@task('maintenance-mode-on')
+cd {{ $path }}
+php artisan down --secret=c2fBxu9p8o
+@endtask
+
+@task('optimize')
+cd {{ $path }}
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+@endtask
+
+@task('build-assets')
+cd {{ $path }}
+npm run build
+@endtask
+
+@task('migrate-database')
+cd {{ $path }}
+php artisan migrate --force
+@endtask
+
+@task('reload-octane-server')
+cd {{ $path }}
+php artisan octane:reload
+@endtask
+
+@task('restart-queue-workers')
+cd {{ $path }}
+php artisan queue:restart
+@endtask
+
+@task('maintenance-mode-off')
+cd {{ $path }}
+php artisan up
+@endtask
