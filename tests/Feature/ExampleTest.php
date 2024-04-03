7f5ba7383aa85864b3881cda7d8b0f956ea7907b
Dasun Tharanga <hello@dasun.dev>
2024-04-03 14:07:09 +0530

    test: use database migrations

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+uses(DatabaseMigrations::class);
+
 it('returns a successful response', function () {
     $response = $this->get('/');
 
