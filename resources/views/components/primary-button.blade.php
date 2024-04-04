9aa556ec4e8e3c0f2431254093cf0c66c57197e5
Dasun Tharanga <hello@dasun.dev>
2024-04-04 19:29:01 +0530

    fix: text size

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,6 +1,6 @@
 @props([
     'type' => 'button'
 ])
-<button type="button" {{ $attributes->twMerge("text-sm md:text-md lg:text-lg font-semibold bg-lime-600 px-5 py-2 text-white rounded-xl shadow hover:bg-lime-700 transition-colors duration-300") }} {{ $attributes }} >
+<button type="button" {{ $attributes->twMerge("md:text-md lg:text-lg font-semibold bg-lime-600 px-5 py-2 text-white rounded-xl shadow hover:bg-lime-700 transition-colors duration-300") }} {{ $attributes }} >
     {{ $slot }}
 </button>
