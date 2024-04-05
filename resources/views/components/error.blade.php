d73f80b5945805a2fd61549b65c2fda7399a7e97
Dasun Tharanga <hello@dasun.dev>
2024-04-05 11:21:07 +0530

    fix: error padding

--- resources/views/components/error.blade.php
+++ resources/views/components/error.blade.php
@@ -1,4 +1,4 @@
 <div {{ $attributes->twMerge("overflow-x-scroll no-scrollbar p-5 rounded-xl shadow transition-colors") }}>
-    <div class="text-purple-800 font-medium  text-sm font-exception">{{ $error->exception }}</div>
-    <h3 class="text-md whitespace-nowrap sm:text-lg leading-none font-medium mt-2">{{ $error->title }}</h3>
+    <div class="text-purple-800 font-medium text-sm font-exception">{{ $error->exception }}</div>
+    <h3 class="text-md whitespace-nowrap sm:text-lg leading-none font-medium mt-3">{{ $error->title }}</h3>
 </div>
