3d7e273e76021f5e63431e513c52e015c412634a
Dasun Tharanga <hello@dasun.dev>
2024-04-05 13:44:42 +0530

    feat: add sitemap

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Error;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+
+class GenerateSitemap extends Command
+{
+    protected $signature = 'sitemap:generate';
+
+    protected $description = 'Generate the sitemap.';
+
+    public function handle(): void
+    {
+        Log::alert('Generating sitemap...');
+
+        $errors = Error::approved()
+            ->pluck('slug')
+            ->map(fn ($slug) => Url::create("/error/$slug"));
+
+        Sitemap::create()
+            ->add($errors)
+            ->writeToFile(public_path('sitemap.xml'));
+
+        Log::alert('Sitemap generated successfully.');
+    }
+}
