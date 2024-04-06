9ccffc82c47a4d44ac59445c7812b2e1f75e7fe0
Dasun Tharanga <hello@dasun.dev>
2024-04-06 12:26:57 +0530

    feat: implement algolia docsearch

---
+++ resources/js/docsearch.js
@@ -0,0 +1,11 @@
+import docsearch from '@docsearch/js';
+
+import '@docsearch/css';
+
+docsearch({
+    container: '#search',
+    appId: 'ZZSBJRTP9N',
+    indexName: 'laravel-errors',
+    apiKey: 'afe4b786abd24d3123d50c7ff21e544d',
+    placeholder: 'Search'
+});
